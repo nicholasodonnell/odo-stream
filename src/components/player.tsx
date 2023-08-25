94e2a620f65a2a55514823167727d033b8c71385
Nicholas O'Donnell <nicholas@nicholasodonnell.com>
2023-08-25 00:19:26 -0400

    Add more HLS options

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -11,8 +11,19 @@ export type PlayerProps = {
 }
 
 const hlsConfig = {
-  liveDurationInfinity: true,
-  lowLatencyMode: true,
+  autoStartLoad: true, // auto play on load
+  backBufferLength: 0, // eliminate back buffer
+  enableWorker: true, // use workers
+  initialLiveManifestSize: 2, // preload 2 chunks before autostart
+  liveDurationInfinity: true, // instructs browser that video is live
+  liveMaxLatencyDuration: 10, // if higher than this, adujst to liveSyncDuration
+  liveSyncDuration: 3, // how close to live to target? shorter than 3sec causes frequent buffering issues
+  lowLatencyMode: true, // enable low latency mode
+  maxBufferLength: 10, // limit forward buffer
+  maxLiveSyncPlaybackRate: 2, // if running behind, speed up video
+  nudgeMaxRetry: 5, // increase retries before buffer stalled
+  progressive: true, // use fetch instead of xhr
+  testBandwidth: false, // disable auto bandwidth estimation
 }
 
 export function Player({ className, src }: PlayerProps): React.ReactNode {
