42b33a401cae354823c8275cea4208f0df8e4ee2
Nicholas O'Donnell <nicholas@nicholasodonnell.com>
2023-12-17 18:19:42 -0500

    Set more secuirty headers

--- src/actions/setGlobalHeaders.ts
+++ src/actions/setGlobalHeaders.ts
@@ -12,4 +12,23 @@ export function setGlobalHeaders(
   // Cache
   response.headers.set('Cache-Control', 'no-cache, no-store, must-revalidate')
   response.headers.set('Pragma', 'no-cache')
+
+  // Content Security Policy
+  response.headers.set('Content-Security-Policy', 'self')
+
+  // X-Content-Type-Options
+  response.headers.set('X-Content-Type-Options', 'nosniff')
+
+  // Referrer Policy
+  response.headers.set('Referrer-Policy', 'no-referrer')
+
+  // Strict-Transport-Security
+  response.headers.set('Strict-Transport-Security', 'max-age=63072000; preload')
+
+  // X-Frame-Options
+  response.headers.set('X-Frame-Options', 'DENY')
+
+  // Cross-Origin-Embedder-Policy and Cross-Origin-Opener-Policy
+  response.headers.set('Cross-Origin-Embedder-Policy', 'require-corp')
+  response.headers.set('Cross-Origin-Opener-Policy', 'same-origin')
 }
